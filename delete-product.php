48285184fd811d32b33ef2573d161d8e59cb324a
Paul <paultulod@pm.me>
2022-03-25 23:59:06 +0800

    fix delete product

--- delete-product.php
+++ delete-product.php
@@ -2,7 +2,7 @@
 include('config.php');
 
 $prodID = $_GET['prodID'];
-$sql = "UPDATE product SET Is_Deleted = '1' WHERE ProductID = $prodID";
+$sql = "UPDATE product SET IsDeleted = b'1' WHERE ProductID = $prodID";
 mysqli_query($con, $sql) or die(mysql_error());
 echo "<script> window.alert('Deleted Successfully')</script>";
 echo '<script>window.location.href = "admin-products.php"</script>';
