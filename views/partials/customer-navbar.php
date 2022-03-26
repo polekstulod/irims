8e700e50d830404c959c2a0597df1c571d908427
Paul <paultulod@pm.me>
2022-03-26 18:45:10 +0800

    remove cart

--- views/partials/customer-navbar.php
+++ views/partials/customer-navbar.php
@@ -25,11 +25,6 @@
                 $user_ID = $row[0];
             }
             ?>
-            <a href="cart.php?user_id=<?php echo $user_ID ?>">
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-bag-fill icon-color">
-                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z"></path>
-                </svg>
-            </a>
         </div>
     </nav>
 </div>
